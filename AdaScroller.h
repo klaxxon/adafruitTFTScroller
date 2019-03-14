18758cd747b6b9da21ce5f6f4c114f58b486818e
klaxxon <jon@gettys.org>
2019-03-14 12:59:49 -0500

    Cleanup, text color methods.

--- AdaScroller.h
+++ AdaScroller.h
@@ -4,17 +4,25 @@
 
 class Log2TFT : public Print {
   public:
+  typedef struct RECT {
+    unsigned short x, y, w, h;
+  } RECT;
+  
   Log2TFT(Adafruit_HX8357 *tft);
   void begin(int);
   void setTextSize(int x);
   void setLinePadSize(int x);
   void config(int rotation, int fontsize);
-  void scrollAddress(uint16_t VSP);
-  void setupScrollArea(uint16_t TFA, uint16_t BFA);
-  void scroll();
+  void setTextColor(unsigned short col) {textColor_ = col; }
+  void setBackgroundColor(unsigned short col) {bgroundColor_ = col; }
   size_t write(uint8_t x);
+  RECT getCurrentLineBounds();
 
   private:
+    void scrollAddress(uint16_t VSP);
+    void setupScrollArea(uint16_t TFA, uint16_t BFA);
+    void scroll();
+
     Adafruit_HX8357 *tft_;
     char buf_[64];
     unsigned char bsize_;
@@ -23,7 +31,8 @@ class Log2TFT : public Print {
     unsigned char charsize_;
     unsigned short dwidth_, dheight_;
     unsigned char yspacing_;
+    unsigned short textColor_;
+    unsigned short bgroundColor_;
 };
 
 #endif
-
